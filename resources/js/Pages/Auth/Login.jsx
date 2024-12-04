5264476c7e3c4ce4a8de9d345f53b46993cf67d5
Rand <randdevs54@gmail.com>
2024-12-04 09:55:54 +0700

    Login page: Add form to login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,8 +1,94 @@
 export default function () {
     return (
-        <div className="">
-
-            <h1 className="font-bold">INI LOGIN ! </h1>
-        </div>
+        <section className="bg-gray-50 m-0">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <p className="flex items-center mb-6 text-3xl font-bold text-gray-900 ">
+                    PetaNusa
+                </p>
+                <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label
+                                    for="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-1 focus:ring-orange-400 focus:border-orange-400 outline-none block w-full p-2.5 transition-all"
+                                    placeholder="name@company.com"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    for="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-1 focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5 outline-none transition-all "
+                                    required
+                                />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            id="remember"
+                                            aria-describedby="remember"
+                                            type="checkbox"
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-orange-300 cursor-pointer"
+                                            required=""
+                                        />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label
+                                            for="remember"
+                                            className="text-gray-500 cursor-pointer"
+                                        >
+                                            Remember me
+                                        </label>
+                                    </div>
+                                </div>
+                                <a
+                                    href="#"
+                                    className="text-sm font-medium text-orange-600 hover:underline "
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-orange-500 border border-orange-400 hover:bg-orange-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all duration-300"
+                            >
+                                Sign in
+                            </button>
+                            <p className="text-sm font-light text-gray-500">
+                                Don’t have an account yet?{" "}
+                                <a
+                                    href="#"
+                                    className="font-medium text-orange-600 hover:underline "
+                                >
+                                    Sign up
+                                </a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,36 +1,36 @@
 export default function () {
     return (
-        <section class="bg-gray-50 m-0">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <p class="flex items-center mb-6 text-3xl font-bold text-gray-900 ">
+        <section className="bg-gray-50 m-0">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <p className="flex items-center mb-6 text-3xl font-bold text-gray-900 ">
                     PetaNusa
                 </p>
-                <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                            Sign in to your account
+                <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            Register your account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label
                                     for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 "
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
                                 >
-                                    Your email
+                                    Email
                                 </label>
                                 <input
                                     type="email"
                                     name="email"
                                     id="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 "
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-1 focus:ring-orange-400 focus:border-orange-400 outline-none block w-full p-2.5 transition-all"
                                     placeholder="name@company.com"
-                                    required=""
+                                    required
                                 />
                             </div>
                             <div>
                                 <label
                                     for="password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 "
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
                                 >
                                     Password
                                 </label>
@@ -39,50 +39,24 @@ export default function () {
                                     name="password"
                                     id="password"
                                     placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 "
-                                    required=""
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-1 focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5 outline-none transition-all "
+                                    required
                                 />
                             </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input
-                                            id="remember"
-                                            aria-describedby="remember"
-                                            type="checkbox"
-                                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-orange-300"
-                                            required=""
-                                        />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label
-                                            for="remember"
-                                            class="text-gray-500 dark:text-gray-300"
-                                        >
-                                            Remember me
-                                        </label>
-                                    </div>
-                                </div>
-                                <a
-                                    href="#"
-                                    class="text-sm font-medium text-primary-600 hover:underline "
-                                >
-                                    Forgot password?
-                                </a>
-                            </div>
+
                             <button
                                 type="submit"
-                                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                                className="w-full text-orange-500 border border-orange-400 hover:bg-orange-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all duration-300"
                             >
-                                Sign in
+                                Sign Up
                             </button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet?{" "}
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account?{" "}
                                 <a
                                     href="#"
-                                    class="font-medium text-primary-600 hover:underline "
+                                    className="font-medium text-orange-600 hover:underline "
                                 >
-                                    Sign up
+                                    Sign In
                                 </a>
                             </p>
                         </form>
